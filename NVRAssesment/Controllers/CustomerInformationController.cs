6d10f320d2a23f143738a1395e72c8036ce61e1a
ajayv <ajayv@AJAYS-LAPTOP>
2022-10-31 18:27:17 -0400

    Add project files.

---
+++ NVRAssesment/Controllers/CustomerInformationController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using NotNullAttribute = JetBrains.Annotations.NotNullAttribute;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using System.IO;
+
+
+namespace NvrAssessment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerInformationController : ControllerBase
+    {
+
+        private IHostingEnvironment _env;
+        string filename = "Customers.txt";
+        public CustomerInformationController(IHostingEnvironment env)
+        {
+            _env = env; 
+        }
+
+        /// <summary>
+        ///     Gets list of customer information
+        /// </summary>
+        /// <returns>Customer informations</returns>
+        [HttpGet]
+        [NotNull]
+        public IEnumerable<CustomerInformation> Get()
+        {
+
+            // application's base path
+            string contentRootPath = _env.ContentRootPath;
+            string  filePath = contentRootPath + filename;
+
+            var jsonText = System.IO.File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<CustomerInformation>>(jsonText)
+                                      ?? new List<CustomerInformation>();
+        }
+
+        /// <summary>
+        ///     Provides customer information
+        /// </summary>
+        /// <param name="customerInformation">Customer information</param>
+        /// <returns>Customer information</returns>
+        [HttpPost()]
+        [NotNull]
+        public Task CustomerInformation(
+            [FromBody] [NotNull] CustomerInformation customerInformation)
+        {
+
+            string contentRootPath = _env.ContentRootPath;      
+            string filePath = contentRootPath + filename;
+
+            // Reads existing customer information
+            var customerData = System.IO.File.ReadAllText(filePath);
+
+                var customerInfo = JsonConvert.DeserializeObject<List<CustomerInformation>>(customerData)
+                                      ?? new List<CustomerInformation>();
+
+                // Adds new customer information
+                customerInfo.Add(new CustomerInformation()
+                {
+                    FirstName = customerInformation.FirstName,
+                    LastName = customerInformation.LastName,
+                    EmailAddress = customerInformation.EmailAddress,
+                    PhoneNumber = customerInformation.PhoneNumber
+                });
+
+                // Updates customer information
+                customerData = JsonConvert.SerializeObject(customerInfo);
+                System.IO.File.WriteAllText(filePath, customerData);
+            
+            
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ NVRAssesment/CustomerInformation.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NvrAssessment
+{
+    /// <summary>
+    ///     Represents customer information
+    /// </summary>
+    public sealed class CustomerInformation
+    {   
+        /// <summary>
+        ///     First name
+        /// </summary>
+        [CanBeNull]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        ///     Last name
+        /// </summary>
+        [CanBeNull]
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///     Email address
+        /// </summary>
+        [NotNull]
+        [JsonProperty(Required = Required.Always)]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        ///     Phone number
+        /// </summary>
+        [CanBeNull]
+        public string PhoneNumber { get; set; }
+
+    
+        public override bool Equals(object obj) => obj is CustomerInformation information
+            && this.FirstName == information.FirstName
+            && this.LastName == information.LastName
+            && this.EmailAddress == information.EmailAddress
+            && this.PhoneNumber == information.PhoneNumber;
+
+        public override int GetHashCode() => HashCode.Combine(this.FirstName, this.LastName, this.EmailAddress, this.PhoneNumber);
+    }
+}
